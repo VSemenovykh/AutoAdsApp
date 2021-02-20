df6ea0b9e9541d5d982bb15b4d80262b20805c07
Vladislav Semenovykh <71659506+VSemenovykh@users.noreply.github.com>
2021-02-20 14:14:11 +0300

    update  Drive.java

--- src/main/java/ru/ncedu/model/Drive.java
+++ src/main/java/ru/ncedu/model/Drive.java
@@ -1,8 +1,8 @@
-package ru.ncedu.model;	
+package ru.ncedu.model;
 
-public enum Drive {	
-     RWD     //REARWHEELDRIVE	
-    ,FRWD   //FRONTWHEELDRIVE	
-    ,FWD;   //FOURWHEELDRIVE	
+public enum Drive {
+     RWD     //REARWHEELDRIVE
+    ,FRWD   //FRONTWHEELDRIVE
+    ,FWD;   //FOURWHEELDRIVE
 
 }
