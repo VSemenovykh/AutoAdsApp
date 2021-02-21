0b2bfc10f68d18c1f0d6afad167f4d8e678796c5
VSemenovykh <v.semenovykh98@gmail.com>
2021-02-21 14:16:05 +0300

    deleted all setters and getters added annotations Data, AllArgsConstructor, NoArgsConstructor from lombok

--- src/main/java/ru/ncedu/model/Motor.java
+++ src/main/java/ru/ncedu/model/Motor.java
@@ -1,31 +1,14 @@
 package ru.ncedu.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Motor {
     private int id;
     private Fuel motorType;
     private double volume;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Fuel getMotorType() {
-        return motorType;
-    }
-
-    public void setMotorType(Fuel motorType) {
-        this.motorType = motorType;
-    }
-
-    public double getVolume() {
-        return volume;
-    }
-
-    public void setVolume(double volume) {
-        this.volume = volume;
-    }
 }
