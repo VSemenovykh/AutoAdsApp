d85d393720944d6ff8cbe4839777b74f950989a1
VSemenovykh <v.semenovykh98@gmail.com>
2021-02-24 19:10:28 +0300

    deleted entity, model, repository

--- src/main/java/ru/ncedu/AutoAdsApplication.java
+++ src/main/java/ru/ncedu/AutoAdsApplication.java
@@ -1,40 +1,20 @@
 package ru.ncedu;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.ncedu.entity.User;
-import ru.ncedu.repository.UserRepository;
 
+@Slf4j
 @SpringBootApplication
 public class AutoAdsApplication   implements CommandLineRunner {
 
-    private static final Logger log = LoggerFactory.getLogger(AutoAdsApplication.class);
-
-    @Autowired
-    private UserRepository repository;
     public static void main(String[] args) {
         SpringApplication.run(AutoAdsApplication.class, args);
-
     }
+
     @Override
     public void run(String... args) {
         log.info("StartApplication...");
-
-        repository.save(new User("Pety","client"));
-        repository.save(new User("Mary","admin"));
-        repository.save(new User("Sasha","client"));
-
-        System.out.println("\nfindAll()");
-        repository.findAll().forEach(x -> System.out.println(x));
-
-        System.out.println("\nfindById(1L)");
-        repository.findById(1l).ifPresent(x -> System.out.println(x));
-
-        System.out.println("\nfindByName('Node')");
-        repository.findByName("Mary").forEach(x -> System.out.println(x));
     }
 }
--- src/main/java/ru/ncedu/model/Fuel.java
+++
@@ -1,7 +0,0 @@
-package ru.ncedu.model;
-
-public enum Fuel {
-    DIESEL,
-    ELECTRIC,
-    GASOLINE;
-}
