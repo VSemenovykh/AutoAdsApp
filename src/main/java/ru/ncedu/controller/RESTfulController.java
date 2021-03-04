e4cc72c90fbed5de49e43527e675dde858fe38e1
VSemenovykh <v.semenovykh98@gmail.com>
2021-03-04 12:09:20 +0300

    changed RESTfullController

--- src/main/java/ru/ncedu/controller/RESTfulController.java
+++ src/main/java/ru/ncedu/controller/RESTfulController.java
@@ -1,11 +1,9 @@
 package ru.ncedu.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
 import ru.ncedu.entity.Auto;
 import ru.ncedu.entity.Brand;
 import ru.ncedu.entity.Motor;
@@ -19,7 +17,8 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = {"http://localhost:8080"})
+@CrossOrigin(origins = {"http://localhost:4200"})
+@RequestMapping({"/auto-all"})
 public class RESTfulController {
 
     private final AutoService autoService;
@@ -28,8 +27,8 @@ public class RESTfulController {
 
     private final MotorService motorService;
 
-    @GetMapping("/auto-all")
-    public ResponseEntity<List<AutoJoin>> getAllAuto() {
+    @GetMapping
+    public List<AutoJoin> getAllAuto() {
 
         List<Auto> auto = autoService.findAll();
         List<AutoJoin> listAutoJoin = new ArrayList<>();
@@ -53,45 +52,49 @@ public class RESTfulController {
             volume = motorService.findById(auto.get(i).getIdMotor()).getVolume();
 
             autoJoin = new AutoJoin(auto.get(i).getId(), brandName, modelName, year, auto.get(i).getColor()
-                                   ,auto.get(i).getPrice(), motorType, volume, auto.get(i).getDriveType()
-                                   ,auto.get(i).getTransmissionType(), auto.get(i).getBodyStyleType());
+                                    ,auto.get(i).getPrice(), motorType, volume, auto.get(i).getDriveType()
+                                    ,auto.get(i).getTransmissionType(), auto.get(i).getBodyStyleType());
 
             listAutoJoin.add(autoJoin);
         }
 
-        return new ResponseEntity<List<AutoJoin>>(listAutoJoin, HttpStatus.OK);
+        return listAutoJoin;
     }
 
-    @PostMapping("/auto-all/add")
-    public ResponseEntity<Void> createAuto(@RequestBody Auto auto, UriComponentsBuilder builder) {
+    @PostMapping(path = {"/add"})
+    public AutoJoin createAuto(@RequestBody Auto auto) {
 
         auto.setDriveType(auto.getDriveType());
         auto.setTransmissionType(auto.getTransmissionType());
         auto.setBodyStyleType(auto.getBodyStyleType());
         Auto newAuto = autoService.save(auto);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(builder.path("/add?id={id}").buildAndExpand(autoService.save(auto)).toUri());
+        Brand brand = brandService.findById(auto.getIdBrand());
+        Motor motor = motorService.findById(auto.getIdMotor());
 
-        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+        AutoJoin autoJoin = new AutoJoin(newAuto.getId(), brand.getNameBrand(), brand.getNameModel(), brand.getYear()
+                                        ,newAuto.getColor(), newAuto.getPrice(), motor.getMotorType(), motor.getVolume()
+                                        ,newAuto.getDriveType(), newAuto.getTransmissionType(), newAuto.getBodyStyleType());
+
+        return autoJoin;
     }
 
-    @GetMapping("/auto-all/{autoId}")
-    public ResponseEntity<AutoJoin> getAutoById(@PathVariable long autoId) {
+    @GetMapping(path = {"/{id}"})
+    public AutoJoin getAutoById(@PathVariable("id") long autoId) {
 
         Auto auto = autoService.findById(autoId);
         Brand brand = brandService.findById(auto.getIdBrand());
         Motor motor = motorService.findById(auto.getIdMotor());
 
-        AutoJoin autoJoin = new AutoJoin(auto.getId(), brand.getNameBrand(), brand.getNameModel(), brand.getYear(),
-                auto.getColor(), auto.getPrice(), motor.getMotorType(), motor.getVolume(),
-                auto.getDriveType(), auto.getTransmissionType(), auto.getBodyStyleType());
+        AutoJoin autoJoin = new AutoJoin(auto.getId(), brand.getNameBrand(), brand.getNameModel(), brand.getYear()
+                                         ,auto.getColor(), auto.getPrice(), motor.getMotorType(), motor.getVolume()
+                                         ,auto.getDriveType(), auto.getTransmissionType(), auto.getBodyStyleType());
 
-        return new ResponseEntity<AutoJoin>(autoJoin, HttpStatus.OK);
+        return autoJoin;
     }
 
-    @PutMapping("/auto-all/{autoId}/edit")
-    public ResponseEntity<Auto> updateAuto(@RequestBody Auto auto, @PathVariable Long autoId) {
+    @PutMapping("/{id}")
+    public AutoJoin updateAuto(@RequestBody Auto auto, @PathVariable("id") Long autoId) {
 
         auto.setId(autoId);
         auto.setIdBrand(auto.getIdBrand());
@@ -103,17 +106,27 @@ public class RESTfulController {
         auto.setBodyStyleType(auto.getBodyStyleType());
         autoService.update(auto);
 
-        return new ResponseEntity<Auto>(auto, HttpStatus.OK);
+        Brand brand = brandService.findById(auto.getIdBrand());
+        Motor motor = motorService.findById(auto.getIdMotor());
+
+        AutoJoin autoJoin = new AutoJoin(auto.getId(), brand.getNameBrand(), brand.getNameModel(), brand.getYear(),
+                auto.getColor(), auto.getPrice(), motor.getMotorType(), motor.getVolume(),
+                auto.getDriveType(), auto.getTransmissionType(), auto.getBodyStyleType());
+
+        return autoJoin;
     }
 
-    @DeleteMapping(value = "/auto-all/{autoId}/delete")
-    public ResponseEntity deleteAuto(@PathVariable long autoId) {
+    @DeleteMapping("/{id}") //value = "/auto-all/{autoId}/delete"
+    public ResponseEntity deleteAuto(@PathVariable("id") Long autoId) {
 
         Auto auto = autoService.findById(autoId);
         auto.setIdBrand(0L);
         auto.setIdMotor(0L);
 
         autoService.delete(autoId);
+
+        Brand brand = brandService.findById(auto.getIdBrand());
+        Motor motor = motorService.findById(auto.getIdMotor());
 
         return ResponseEntity.ok().body("Auto with ID : " + autoId + " deleted with success!");
     }
